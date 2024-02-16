b04b9383b6c3b2c7aa81229c06ea6f289879941a
Aleksandr Gabelko <aleksandrgabelko@gmail.com>
2024-02-16 21:31:40 +0600

    Условие и ввод данных первой задачи

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+
+Console.WriteLine("Введите значение M: ");
+int mValue = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение N: ");
+int nValue = int.Parse(Console.ReadLine()!);
