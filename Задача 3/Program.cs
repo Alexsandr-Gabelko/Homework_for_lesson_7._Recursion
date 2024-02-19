c3858ee75154c41d6c8432d08847a11d9d04bcbb
Aleksandr Gabelko <aleksandrgabelko@gmail.com>
2024-02-19 16:07:59 +0600

    Рабочий код Задача 3

--- Задача 3/Program.cs
+++ Задача 3/Program.cs
@@ -1,3 +1,25 @@
 ﻿// Задача 3: Задайте произвольный массив. Выведете
 // его элементы, начиная с конца. Использовать
 // рекурсию, не использовать циклы.
+
+void PrintFromEnd(int[] numbers, int index)
+{ 
+    if (index == 0)
+        {
+        System.Console.Write(numbers[index]);
+        return ;
+        }
+    System.Console.Write(numbers[index] + " ");
+    index --; 
+    PrintFromEnd(numbers, index);
+    return;
+}
+
+Random rnd = new Random();
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+    {
+    array[i] = rnd.Next(0, 101);
+    }
+// Console.WriteLine(string.Join(", ", array)); 
+PrintFromEnd(array, array.Length - 1);
